bdfbc921083111ccbb28f546e34f95c8ac6bed46
Asif-vai <mr3366533@gmail.com>
2025-12-06 17:59:39 +0600

    firebase initialize

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey:import.meta.VITE_apiKey ,
+  authDomain:import.meta.VITE_authDomain ,
+  projectId:import.meta.VITE_projectId,
+  storageBucket:import.meta.VITE_storageBucket ,
+  messagingSenderId:import.meta.VITE_messagingSenderId ,
+  appId:import.meta.VITE_appId ,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,16 +1,26 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Auth/Login/Login";
+import Register from "../pages/Auth/Register/Register";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    Component:RootLayout,
-    children:[
+    Component: RootLayout,
+    children: [
       {
-        index:true,
-        Component:Home
-      }
-    ]
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    Component: Login,
+  },
+  {
+    path: "/register",
+    Component: Register,
   },
 ]);
